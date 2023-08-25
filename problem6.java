b7bdcf50aa1e103f2280e2287b4fd903db96d596
Daniel Duy Mai <70843762+DanielDuy@users.noreply.github.com>
2023-08-25 23:06:54 +0200

    Create problem6.java

---
+++ problem6.java
@@ -0,0 +1,21 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        double sumSqr = 0;
+
+        for (int i = 1; i < 101; i++) {
+            sumSqr += Math.pow(i, 2);
+        }
+
+        double sqrSum = 0;
+
+        for (int i = 1; i < 101; i++) {
+            sqrSum += i;
+        }
+
+        sqrSum = Math.pow(sqrSum, 2);
+
+        System.out.printf("%.0f%n", sqrSum-sumSqr);
+    }
+}
