5fec0bf41bcf10a5f6fea6b04c90f2d1c510b7db
Daniel Duy Mai <70843762+DanielDuy@users.noreply.github.com>
2023-08-25 22:55:17 +0200

    Create problem5.java

---
+++ problem5.java
@@ -0,0 +1,26 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int min = 20;
+
+        while (true)
+        {
+            boolean noRemainder = true;
+
+            for (int i = 1; i < 21; i++) {
+                if (min % i != 0) {
+                    noRemainder = false;
+                    break;
+                }
+            }
+
+            if (noRemainder) {
+                System.out.println(min);
+                break;
+            }
+
+            min++;
+        }
+    }
+}
