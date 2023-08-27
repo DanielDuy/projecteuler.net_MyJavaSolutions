1fd8df24141facfab0a12c6745b4ffe23d3e4c52
Daniel Duy Mai <70843762+DanielDuy@users.noreply.github.com>
2023-08-27 16:18:55 +0200

    Create problem7.java

---
+++ problem7.java
@@ -0,0 +1,28 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int primeCounter = 0;
+        int number = 2;
+        int recentPrime = 0;
+        while (primeCounter < 10001)
+        {
+            boolean prime = true;
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime)
+            {
+                primeCounter++;
+                recentPrime = number;
+            }
+            number++;
+        }
+        System.out.println(recentPrime);
+    }
+}
